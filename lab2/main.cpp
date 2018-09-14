821fae8d3ee449aab138771d508afb49a2f18e0b
Konstantin Zboichik <kostya.zboichik@gmail.com>
2018-09-14 09:37:18 +0300

    Lab 2 init

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using namespace std;
 
 int main() {
-    std::cout << "lab2" << std::endl;
+
+    int a;
+    cout << "hello world" << std::endl;
+    cout << "привет мир";
+    cin >> a;
+    cout << std::hex << a;
+    cout << a;
+
     return 0;
 }
